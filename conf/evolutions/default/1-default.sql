e19285feb30c90c1acda12cd18d456a58cd0e0f3
matejcik neasi <ja@matejcik.cz>
2012-10-22 03:09:43 +0200

    postgresql dialect. it's a serious big boy database. it's picky.

--- conf/evolutions/default/1-default.sql
+++ conf/evolutions/default/1-default.sql
@@ -1,45 +1,45 @@
 # --- !Ups
 
 CREATE TABLE users (
-	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(50) NOT NULL UNIQUE,
-	admin BIT NOT NULL DEFAULT 0
+	id bigserial NOT NULL PRIMARY KEY,
+	name varchar(50) NOT NULL UNIQUE,
+	admin boolean NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE entities (
-	id BIGINT(20) NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) UNIQUE,
-	comment TEXT NOT NULL DEFAULT ''
+	id bigserial NOT NULL PRIMARY KEY,
+	name varchar(255) UNIQUE,
+	comment text NOT NULL DEFAULT ''
 );
 
 CREATE TABLE character_component (
-	id BIGINT(20) NOT NULL REFERENCES entities(id),
-	player INT NOT NULL REFERENCES users(id),
-	name VARCHAR(255) NOT NULL UNIQUE,
-	online BIT
+	id bigint NOT NULL PRIMARY KEY REFERENCES entities(id),
+	player int NOT NULL REFERENCES users(id),
+	name varchar(255) NOT NULL UNIQUE,
+	online boolean
 );
 
 CREATE TABLE outpost_component (
-	id BIGINT(20) NOT NULL REFERENCES entities(id),
-	name VARCHAR(255) NOT NULL UNIQUE,
-	x DOUBLE NOT NULL,
-	y DOUBLE NOT NULL
+	id bigint NOT NULL PRIMARY KEY REFERENCES entities(id),
+	name varchar(255) NOT NULL UNIQUE,
+	x double precision NOT NULL,
+	y double precision NOT NULL
 );
 
 CREATE TABLE plane_component (
-	id BIGINT(20) NOT NULL REFERENCES entities(id),
-	x DOUBLE NOT NULL,
-	y DOUBLE NOT NULL,
-	location_id BIGINT(20) REFERENCES entities(id),
-	fuel DOUBLE NOT NULL
+	id bigint NOT NULL PRIMARY KEY REFERENCES entities(id),
+	x double precision NOT NULL,
+	y double precision NOT NULL,
+	location_id bigint REFERENCES entities(id),
+	fuel double precision NOT NULL
 );
 
 CREATE TABLE in_flight_component (
-	id BIGINT(20) NOT NULL REFERENCES entities(id),
-	target_x DOUBLE NOT NULL,
-	target_y DOUBLE NOT NULL,
-	target_id BIGINT(20) REFERENCES entities(id),
-	time_to_land DATETIME
+	id bigint NOT NULL PRIMARY KEY REFERENCES entities(id),
+	target_x double precision NOT NULL,
+	target_y double precision NOT NULL,
+	target_id bigint REFERENCES entities(id),
+	time_to_land timestamp with time zone
 );
 
 INSERT INTO entities (name, comment) VALUES ('OUTPOST_ZERO', 'starting point');
@@ -48,10 +48,12 @@ INSERT INTO outpost_component (id, name, x, y)
 
 # --- !Downs
 
-DROP TABLE users;
-
-DROP TABLE entities;
+-- order is important, otherwise we violate foreign keys
+-- if needed, use DROP TABLE name CASCADE;
 DROP TABLE character_component;
 DROP TABLE outpost_component;
 DROP TABLE plane_component;
 DROP TABLE in_flight_component;
+
+DROP TABLE users;
+DROP TABLE entities;
